7fa49fc61df220419b6771912fb500be45bf95d2
shintarowfresh <shintarowfresh@gmail.com>
2015-02-24 23:55:19 +0900

    gulpでコンパスろうと思ったけど、ダメだった。

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ var gulputil = require('gulp-util');
 var sass = require('gulp-ruby-sass');
 var pleeease = require('gulp-pleeease');
 var plumber = require('gulp-plumber');
+var compass = require('gulp-compass');
 
 // Sass(SCSS)ビルドタスク
 gulp.task('sass', function () {
@@ -25,7 +26,17 @@ gulp.task('ple', function() {
     .pipe(gulp.dest('./css/'));
 });
 
+gulp.task('compass', function() {
+    gulp.src('./saaa/*.scss')
+    .pipe(compass({
+        config_file: './config.rb',
+    }))
+    .pipe(gulp.dest('app/assets/temp'));
+});
+
 gulp.task('watch', function() {
-    gulp.watch(['./sass/*.scss'], ['sass']);
+    gulp.watch(['./sass/*.scss'], ['compass']);
     return gulp.watch(['./css/*.css'], ['ple']);
 });
+
+gulp.task('default',['compass']);
