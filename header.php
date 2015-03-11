f543a1e83178143fb81692ef69d532fe122f962d
阿部信太郎 <shintarowfresh@gmail.com>
2015-03-11 18:45:32 +0900

    apple-touch-iconを実装

--- header.php
+++ header.php
@@ -22,6 +22,13 @@
 <title><?php wp_title(); ?></title>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 
+<link rel="apple-touch-icon" href="apple-touch-icon.png">
+<link rel="apple-touch-icon" sizes="72x72" href="apple-touch-icon-72x72.png">
+<link rel="apple-touch-icon" sizes="76x76" href="apple-touch-icon-76x76.png">
+<link rel="apple-touch-icon" sizes="114x114" href="apple-touch-icon-114x114.png">
+<link rel="apple-touch-icon" sizes="120x120" href="apple-touch-icon-120x120.png">
+<link rel="apple-touch-icon" sizes="144x144" href="apple-touch-icon-144x144.png">
+<link rel="apple-touch-icon" sizes="152x152" href="apple-touch-icon-152x152.png">
 
 
 <?php wp_head(); ?>
