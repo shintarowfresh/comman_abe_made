8532523a53825447b7ff3cac67a5da00847cf464
阿部信太郎 <shintarowfresh@gmail.com>
2015-03-30 22:36:21 +0900

    投稿者で条件分岐

--- content.php
+++ content.php
@@ -216,6 +216,11 @@ $author_badge = get_field('author_badge', 'user_'. $author_id ); // image field,
 
                 <p>この記事は「<?php the_category(' '); ?>」カテゴリーです。</p>
 
+                <div class="each-auther">
+                <?php if ($author_id == "7" ) :?>
+                <?php endif ;?>
+                </div>
+
                 <div class="fb-like scale" data-layout="button_count" data-action="like" data-show-faces="true" data-share="false"></div>
 
                 <?php else: ?>
