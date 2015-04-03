f21b2af1ed388c269c84a02e3397c6a6c28c19f3
阿部信太郎 <shintarowfresh@gmail.com>
2015-04-03 17:21:07 +0900

    ブログ用のトップページを調整
    注目記事が表示できるように。
    サムネイルが大きく表示出来るように。

---
+++ category-staff-blog.php
@@ -0,0 +1,76 @@
+<?php
+/**
+ * アーカイブ用のテンプレート
+ *
+ *
+ */
+get_header(); ?>
+
+<div class="content content_width" role="main">
+
+  <?php if( !is_mobile() ) :?>
+
+    <div class="feat-posts inner">
+
+      <ul>
+        <?php $args = array(
+          'posts_per_page'=>'4',
+    'tag' => 'feat',
+); ?>
+
+<?php $query = new WP_Query( $args ); ?>
+<?php if( $query->have_posts() ) : ?>
+    <?php while ( $query->have_posts() ) : $query->the_post(); ?>
+
+      <li>
+        <a href="<?php the_permalink(); ?>">
+          <div class="feat-posts__thum">
+            <?php the_post_thumbnail('single-eye'); ?>
+          </div>
+
+        <div class="feat-posts__title">
+          <h2 itemprop="name" class="title entry-title"><?php the_title(); ?></h2>
+        </div>
+      </a>
+      </li>
+
+    <?php endwhile; wp_reset_postdata(); ?>
+<?php else : ?>
+    Not Found.
+<?php endif; ?>
+      </ul>
+
+    </div>
+
+<?php endif ; //モバイルの時は出さない ?>
+
+    <div class="three-two">
+        <div class="inner">
+            <div class="main">
+
+                <div class="container">
+
+                    <?php
+                    // カテゴリーアーカイブ用のテンプレート呼び出し
+                    get_template_part( 'content-catstaff' ); ?>
+
+                </div><!--/.container-->
+
+                <div id="next">
+                    <a class="btn nonmover" href="<?php echo next_posts(0, false); ?>">次を読み込む</a>
+                    <img id="loading" src="<?php echo get_template_directory_uri() ?>/img/icon-loading.gif" alt="読み込み中">
+                </div>
+
+            </div><!--/.main-->
+
+            <div class="sub"><!--サイドバー-->
+
+                <?php get_sidebar(); ?>
+
+            </div><!--/.sub-->
+        </div><!--/.inner-->
+    </div><!--/.three-two-->
+
+</div><!--/.content-->
+
+<?php get_footer(); ?>
---
+++ content-catstaff.php
@@ -0,0 +1,93 @@
+<?php
+/**
+ * スタッフブログアーカイブ用テンプレート
+ */
+ ?>
+<?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
+
+    <article <?php post_class(); ?>>
+
+      <div class="content-thum">
+
+        <?php if(has_post_thumbnail()): ?>
+
+          <a href="<?php the_permalink(); ?>">
+          <?php if( is_mobile() ) :?>
+          <?php the_post_thumbnail(); ?>
+          <?php else :?>
+          <?php the_post_thumbnail('single-eye'); ?>
+          <?php endif ;?>
+          </a>
+
+            <?php
+            $days = 1;
+            $today = date_i18n('U');
+            $entry = get_the_time('U');
+            $elapsed = date('U',($today - $entry)) / 43200;
+            if( $days > $elapsed ){
+                echo '<span class="new">本日更新！</span>';
+            }
+            ?>
+
+        <?php else :?>
+
+            <a href="<?php the_permalink(); ?>"><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/no-thum.png" alt="サムネイルはありません"></a>
+
+            <?php
+            $days = 1;
+            $today = date_i18n('U');
+            $entry = get_the_time('U');
+            $elapsed = date('U',($today - $entry)) / 86400;
+            if( $days > $elapsed ){
+                echo '<span class="new">本日更新！</span>';
+            }
+            ?>
+
+        <?php endif ;?>
+
+      </div><!--/.content-thum-->
+
+        <div itemscope itemtype="http://schema.org/Article" class="content-main">
+
+            <header>
+
+                <?php
+                $author_id = get_the_author_meta( 'ID' );
+                $author_badge = get_field('author_badge', 'user_'. $author_id );
+                ?>
+
+                <time itemprop="datePublished" content="<?php the_time( 'Y-n-j' ); ?>"><span class="entry-date date updated"><?php the_time( 'Y.n.j' ); ?></span></time>
+
+                <span class="auther-name"><i class="fa fa-pencil"></i> <span class="vcard author" itemprop="author" itemscope itemtype="http://schema.org/Person"><span class="fn" itemprop="name"><?php the_author_meta('nickname'); ?></span></span></span>
+
+                <h2 class="title entry-title">
+                    <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+                </h2>
+
+
+                <div class="single__meta-data">
+
+                    <div class="share-count">
+
+                        <span><i class="fa fa-twitter-square"></i><?php if(function_exists('scc_get_share_twitter')) echo scc_get_share_twitter(); ?></span> <span><i class="fa fa-facebook-official"></i><?php if(function_exists('scc_get_share_facebook')) echo scc_get_share_facebook(); ?></span> <span><i class="fa fa-hatena"></i><?php if(function_exists('scc_get_share_hatebu')) echo scc_get_share_hatebu(); ?></span>
+
+                    </div>
+
+                </div>
+
+            </header>
+
+            <section itemprop="articleBody">
+
+                    <?php the_excerpt(); // 記事の抜粋を表示 ?>
+
+            </section>
+
+            <footer>
+            </footer>
+
+        </div><!--/.content-main-->
+
+    </article>
+
+<?php endwhile; // end of the loop. ?>
