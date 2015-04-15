166bcff8c9bf5b89741289a51157e4acae8e8dc7
阿部信太郎 <shintarowfresh@gmail.com>
2015-04-15 19:25:00 +0900

    特に変化のないコンパイル。

--- footer.php
+++ footer.php
@@ -219,7 +219,7 @@
                 breakpoint: 480,
                 settings: {
                     slidesToShow: 1,
-                    slidesToScroll: 1
+                    slidesToScroll: 1,
                 }
             }
             // You can unslick at a given breakpoint now by adding:
